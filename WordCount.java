546e8328dd6838e30a9bf52a1e80ea96e7389cd9
Nicole Huang <42621884+xynicole@users.noreply.github.com>
2021-12-02 21:55:10 -0500

    Add files via upload

---
+++ WordCount.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+
+public class WordCount {
+
+  public static class WordCountMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+    List<String> stopwords = Arrays.asList("he", "she", "they", "the", "a", "an", "are", "you", "of", "is", "and", "or");
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+
+      StringTokenizer itr = new StringTokenizer(value.toString().replaceAll("[^a-zA-Z0-9\\s]", "").toLowerCase());
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        if(!stopwords.contains(word.toString())){
+            context.write(word, one);
+        }
+      }
+    }
+  }
+
+  public static class WordCountReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "TopN");
+    job.setJarByClass(WordCount.class);
+    job.setMapperClass(WordCountMapper.class);
+    job.setCombinerClass(WordCountReducer.class);
+    job.setReducerClass(WordCountReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  
+  }
+}
